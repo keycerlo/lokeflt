311f85302c6c6ace4a4fca3f2e57c245efd34add
keycerlo <153570777+keycerlo@users.noreply.github.com>
2023-12-18 19:09:43 -0300

    Add files via upload

---
+++ IBlazeConnection.ts
@@ -0,0 +1,16 @@
+import { BlazeEventEmitter } from ".";
+
+export interface IBlazeConnection {
+    ev: BlazeEventEmitter;
+    closeSocket: () => void;
+    sendToSocket: (data: any) => void;
+}
+
+export interface IMakeConnectionOptions {
+    needCloseWithCompletedSession?: boolean;
+    timeoutSendingAliveSocket?: number;
+    token?: string;
+    requireNotRepeated?: boolean;
+    type: 'crash' | 'doubles';
+    reconnectCrash?: boolean;
+}
